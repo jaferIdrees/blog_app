19432e245a75ccd9ef4a04b488e6ce2e7855ba3f
jaferIdrees <jaferll@gmail.com>
2022-10-19 22:13:59 +0300

    Add association between Post and Like.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', counter_cache: :posts_counter
   has_many :comments
+  has_many :likes
 
   def recent_comments
     comments.first(5)
