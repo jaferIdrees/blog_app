4d56969522e3cfce184fd98dc9e9fb665b2fbbf6
jaferIdrees <jaferll@gmail.com>
2022-10-20 20:24:03 +0300

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_thanor_equal_to: 0 }
   after_initialize :init
-  
+
   has_many :posts, inverse_of: 'author', foreign_key: 'author_id', counter_cache: :posts_counter
   has_many :comments, inverse_of: 'author', foreign_key: 'author_id'
   has_many :likes, inverse_of: 'author', foreign_key: 'author_id'
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   end
 
   private
+
   def init
     self.posts_counter ||= 0
   end
