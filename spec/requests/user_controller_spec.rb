757cec108796427a26fee839e6ef302cade5bc91
jaferIdrees <jaferll@gmail.com>
2022-10-25 20:40:33 +0300

    Add users show tests

--- spec/requests/user_controller_spec.rb
+++ spec/requests/user_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users controller', type: :request do
-  context 'testing home route' do
+  context 'testing users home route' do
     it 'Should rennder users index page' do
       get '/'
       expect(response).to render_template('index')
@@ -14,6 +14,21 @@ RSpec.describe 'Users controller', type: :request do
 
     it 'http status to be OK' do
       get '/'
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  context 'testing users show route' do
+    before(:example) { get '/users/:id' }
+    it 'Should rennder users show page' do
+      expect(response).to render_template('show')
+    end
+
+    it 'Rendered page should include "Show selected user information"' do
+      expect(response.body).to include('Show selected user information')
+    end
+
+    it 'http status to be OK' do
       expect(response).to have_http_status(:ok)
     end
   end
