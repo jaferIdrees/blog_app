5779e99f4eeb8221a28dd5d42c7f0a7410de499b
jaferIdrees <jaferll@gmail.com>
2022-10-25 20:48:32 +0300

    Add posts show tests

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -15,5 +15,20 @@ RSpec.describe 'Posts controller', type: :request do
     it 'http status to be OK' do
       expect(response).to have_http_status(:ok)
     end
+  end 
+  
+  context 'testing posts show route' do
+    before(:example) { get '/users/:user_id/posts/:id' }
+    it 'Should rennder posts show page' do
+      expect(response).to render_template('show')
+    end
+
+    it 'Rendered page should include "Here is a given post for a given user"' do
+      expect(response.body).to include('Here is a given post for a given user')
+    end
+
+    it 'http status to be OK' do
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
