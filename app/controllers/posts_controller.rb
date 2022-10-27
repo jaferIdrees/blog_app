89dcbc63f237806f12793b6ca2f37233331dda95
jaferIdrees <jaferll@gmail.com>
2022-10-27 17:54:39 +0300

    Add route new_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,10 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    respond_to do |format|
+      format.html { render :new }
+    end
+  end
 end
