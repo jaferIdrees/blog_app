019988da6248d830c991978dab25ab08178578a2
jaferIdrees <jaferll@gmail.com>
2022-10-28 00:54:02 +0300

    Add new_comment form and method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,20 +11,20 @@ class PostsController < ApplicationController
   def new
     post = Post.new
     respond_to do |format|
-      format.html { render :new_post, locals: {post: post} }
+      format.html { render :new_post, locals: { post: post } }
     end
   end
 
   def create
     # new object from params
-    post = Post.new(title: params[:post][:title], text: params[:post][:text], author: current_user )
+    post = Post.new(title: params[:post][:title], text: params[:post][:text], author: current_user)
     # respond_to block
     if post.save
-      flash[:success] = "Post saved successfully"
+      flash[:success] = 'Post saved successfully'
       redirect_to "/users/#{current_user.id}/posts"
     else
-      flash.now[:error] = "Error: Question could not be saved"
-      render inline: "<p>Error: Question could not be saved<p>"
+      flash.now[:error] = 'Error: Post could not be saved'
+      render inline: '<p>Error: Post could not be saved<p>'
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show'
   get '/posts/new', to: 'posts#new', as: 'new_post'
   get '/users/:user_id/posts', to: 'posts#index'
+  get '/users/:user_id/posts/:id/new_comment', to: 'comments#new', as: 'new_comment'
   get '/users/:user_id/posts/:id', to: 'posts#show'
   post '/users/1/posts', to: 'posts#create'
+  post '/users/:user_id/posts/:id', to: 'comments#create'
 end
