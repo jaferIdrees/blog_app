40cb031977d995d676e29b6d0d79cd60bb70ecef
jaferIdrees <jaferll@gmail.com>
2022-11-01 21:53:33 +0300

    Configure devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root '/',to: "users#index"
+  resource :user # <= here
   get '/', to: 'users#index'
   get '/users/:id', to: 'users#show'
   
