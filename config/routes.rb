357608c89316d936316f597c305122ffc2197d31
jaferIdrees <jaferll@gmail.com>
2022-11-02 18:27:54 +0300

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root '/',to: "users#index"
-  resource :user # <= here
-  get '/', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  
-  get '/users/:user_id/posts', to: 'posts#index'
-  get '/users/:user_id/posts/:id/new_comment', to: 'comments#new', as: 'new_comment'
-  get '/users/:user_id/posts/:id', to: 'posts#show'
-  get '/posts/new', to: 'posts#new', as: 'new_post'
-  post '/users/1/posts', to: 'posts#create'
-  post '/users/:user_id/posts/:id', to: 'comments#create'
-  post '/users/:user_id/posts/:id/like', to: 'posts#like'
+  devise_for :user #, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :new, :create, :show] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
+  end
 end
